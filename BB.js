9591e6d2ccefc94353163b989f2622dc479ddced
Graham Bradley <hello@gbradley.com>
2013-06-11 21:29:26 +0200

    Create BB.js

---
+++ BB.js
@@ -0,0 +1,23 @@
+var BB = {
+	
+	regex	:	/\[([^\]]+)\](.*)(?=\[\/\1\])\[\/\1\]/i,
+
+	parseString : function(str){
+
+		var response = [],
+			match;
+		while ((match = str.match(BB.regex))) {
+
+			response.push({
+				type		: 	match[1],
+				text		: 	match[2],
+				children 	: 	BB.parseString(match[2])
+			});
+
+			str = str.replace(match[0], '');
+
+		}
+		return response;
+	}
+
+};
